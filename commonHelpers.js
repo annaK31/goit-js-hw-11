bc993b9feb50f6b8929b959399735af1e055dd28
annaK31 <148232742+annaK31@users.noreply.github.com>
2024-03-20 17:15:13 +0000

    Deploying to gh-pages from @ annaK31/goit-js-hw-11@34a6d212b32318174d033c2987126ab7cff27178 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,22 +1,22 @@
-import{i as n,S as a}from"./assets/vendor-5b791d57.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&l(s)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const c=document.querySelector(".form"),u=document.querySelector(".images-gallery");function m(r){const o="https://pixabay.com",i="/api/",l=new URLSearchParams({key:"42962590-b9bb006e5b1e7f6cfce132ccb",q:r,image_type:"photo",orientation:"horizontal",safesearch:!0}),e=`${o}${i}?${l}`;return fetch(e).then(t=>t.json())}c.addEventListener("submit",r=>{r.preventDefault();const o=r.currentTarget.elements.image.value.trim();m(o).then(i=>{console.log(i);const l=p(i);u.innerHTML=l}).catch(i=>{n.error({color:"red",position:"center",message:"Sorry, there are no images matching your search query. Please try again!"})})});const p=r=>r.map(o=>`<ul>
+import{i as l,S as a}from"./assets/vendor-5b791d57.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();const c=document.querySelector(".form"),u=document.querySelector(".images-gallery");function p(o){const t="https://pixabay.com",s="/api/",i=new URLSearchParams({key:"42962590-b9bb006e5b1e7f6cfce132ccb",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0}),e=`${t}${s}?${i}`;return fetch(e).then(r=>{if(!r.ok)throw new Error(response.status);return r.json()})}c.addEventListener("submit",o=>{o.preventDefault();const t=o.currentTarget.elements.image.value.trim();p(t).then(s=>{const i=m(s.hits);s.hits.length!==0&&(u.innerHTML=i)}).catch(s=>{l.error({color:"red",position:"center",message:"Sorry, there are no images matching your search query. Please try again!"})})});const m=o=>o.map(t=>`<ul>
  <li class="gallery-item">
-        <a class="gallery-link" href="${r.largeImageURL}">
+        <a class="gallery-link" href="${t.largeImageURL}">
           <img
             class="gallery-image"
             width="1280"
             height="152"
-            src="${r.webformatURL}"
-            data-source="${r.largeImageURL}"
-            alt="${r.tags}"
+            src="${t.webformatURL}"
+            data-source="${t.largeImageURL}"
+            alt="${t.tags}"
           />
           <ul class="gallery-description">
-          <li><h3>Likes</h3><p>${r.likes}</p>
+          <li><h3>Likes</h3><p>${t.likes}</p>
           </li>
-          <li><h3>Views</h3><p>${r.views}</p>
+          <li><h3>Views</h3><p>${t.views}</p>
             </li>
-            <li><h3>Comments</h3><p>${r.comments}</p>
+            <li><h3>Comments</h3><p>${t.comments}</p>
               </li>
-              <li><h3>Downloads</h3><p>${r.downloads}</p>
+              <li><h3>Downloads</h3><p>${t.downloads}</p>
                 </li>
           </ul>
         </a>

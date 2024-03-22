0915d7c1849ba58c7f4a6fb8d43a05add270e07d
annaK31 <148232742+annaK31@users.noreply.github.com>
2024-03-22 15:42:26 +0000

    Deploying to gh-pages from @ annaK31/goit-js-hw-11@74ef49d85d39e876b4c9c58e45c43852bc5bccb9 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,24 +1,23 @@
-import{i as l,S as a}from"./assets/vendor-5b791d57.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();const c=document.querySelector(".form"),u=document.querySelector(".gallery");function p(o){const t="https://pixabay.com",s="/api/",i=new URLSearchParams({key:"42962590-b9bb006e5b1e7f6cfce132ccb",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0}),e=`${t}${s}?${i}`;return fetch(e).then(r=>{if(!r.ok)throw new Error(response.status);return r.json()})}c.addEventListener("submit",o=>{o.preventDefault();const t=o.currentTarget.elements.image.value.trim();p(t).then(s=>{const i=f(s.hits);s.hits.length!==0&&(u.innerHTML=i)}).catch(s=>{l.error({color:"red",position:"center",message:"Sorry, there are no images matching your search query. Please try again!"})})});const f=o=>o.map(t=>`
- <li class="gallery-item">
-        <a class="gallery-link" href="${t.largeImageURL}">
+import{i as c,S as u}from"./assets/vendor-5b791d57.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const l=document.querySelector(".form"),f=document.querySelector(".gallery");function p(o){const r="https://pixabay.com",s="/api/",i=new URLSearchParams({key:"42962590-b9bb006e5b1e7f6cfce132ccb",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0}),e=`${r}${s}?${i}`;return fetch(e).then(t=>{if(!t.ok)throw new Error(response.status);return t.json()})}l.addEventListener("submit",o=>{o.preventDefault();const r=o.currentTarget.elements.image.value.trim();p(r).then(s=>{const i=h(s.hits);if(f.innerHTML=i,s.hits.length===0)return error}).catch(s=>{c.error({color:"red",position:"center",message:"Sorry, there are no images matching your search query. Please try again!"})}).finally(()=>l.reset())});const h=o=>o.map(r=>`<li class="gallery-item">
+        <a class="gallery-link" href="${r.largeImageURL}">
           <img
             class="gallery-image"
             width="1280"
             height="152"
-            src="${t.webformatURL}"
-            data-source="${t.largeImageURL}"
-            alt="${t.tags}"
+            src="${r.webformatURL}"
+            data-source="${r.largeImageURL}"
+            alt="${r.tags}"
           />
+          </a>
           <ul class="gallery-description">
-          <li><h3>Likes</h3><p>${t.likes}</p>
+          <li><h3>Likes</h3><p>${r.likes}</p>
           </li>
-          <li><h3>Views</h3><p>${t.views}</p>
+          <li><h3>Views</h3><p>${r.views}</p>
             </li>
-            <li><h3>Comments</h3><p>${t.comments}</p>
+            <li><h3>Comments</h3><p>${r.comments}</p>
               </li>
-              <li><h3>Downloads</h3><p>${t.downloads}</p>
+              <li><h3>Downloads</h3><p>${r.downloads}</p>
                 </li>
           </ul>
-        </a>
-      </li>`).join("");new a(".gallery a",{captionsData:"alt",captionDelay:250,refresh(){}});
+        </li>`).join(""),m={captionsData:"alt"};let a=new u(".gallery a",m);a.on("show.simplelightbox",function(){});a.refresh();
 //# sourceMappingURL=commonHelpers.js.map

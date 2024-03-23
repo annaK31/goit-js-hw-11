09f26a0f7c5aff457e2493448d605b0c20eeeb36
annaK31 <148232742+annaK31@users.noreply.github.com>
2024-03-23 19:15:24 +0200

    updated hw 11

--- src/main.js
+++ src/main.js
@@ -10,21 +10,30 @@ import { imageTemplate } from './js/render-functions';
 export const imgGallery = document.querySelector(".gallery");
 
 export const formEl = document.querySelector('.form');
-formEl.addEventListener('submit', event => {
-    event.preventDefault();
-    const inputValue = event.currentTarget.elements.image.value.trim();
-    imgGallery.innerHTML = '<div class="loader"></div>';
-    getImage(inputValue).then(data =>{
-      const markup = imageTemplate(data.hits);
-       imgGallery.innerHTML = markup; 
-       const galleryCfg = {
+
+const galleryCfg = {
         captionsData: 'alt',
       };
       let lightbox = new SimpleLightbox('.gallery a', galleryCfg);
       lightbox.on('show.simplelightbox', function () {});
+
+formEl.addEventListener('submit', event => {
+    event.preventDefault();
+    const inputValue = event.currentTarget.elements.image.value.trim();
+    imgGallery.innerHTML = '<div class="loader"></div>';
+    
+    getImage(inputValue).then(data =>{
+      const markup = imageTemplate(data.hits);
+       imgGallery.innerHTML = markup; 
        lightbox.refresh();
-      if(data.hits.length === 0){
-     return error;
+       if (data.hits.length === 0) {
+        iziToast.error({
+            maxWidth: '432px',
+            height: '48px',
+            color: 'red',
+            position: 'topRight',
+            message: "Sorry, there are no images matching your search query. Please try again!",
+        })
       }
     })
     .catch(error => {
@@ -37,7 +46,7 @@ formEl.addEventListener('submit', event => {
       })
         })
   .finally(() => {
-   return formEl.reset()
+    formEl.reset()
   })
   });
 
